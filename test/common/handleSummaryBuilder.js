51cf82abf3120f7e68ddbca94fa2e1473c316f13
DanieleRanaldo <Daniele.Ranaldo@nttdata.com>
2025-09-16 16:26:48 +0200

    [RDB-196] fix summary

--- test/common/handleSummaryBuilder.js
+++ test/common/handleSummaryBuilder.js
@@ -1,11 +1,7 @@
-import {
-    jUnit,
-    textSummary,
-} from 'https://jslib.k6.io/k6-summary/0.0.3/index.js'
+import { jUnit, textSummary } from 'https://jslib.k6.io/k6-summary/0.0.3/index.js'
 import { htmlReport } from 'https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js'
 import { coalesce } from './utils.js'
 import { CONFIG } from './dynamicScenarios/envVars.js'
-import rampingArrivalRate from './dynamicScenarios/scenarios/rampingArrivalRate.js'
 import rampingGrowingArrivalRate from './dynamicScenarios/scenarios/rampingGrowingArrivalRate.js'
 
 const outputDir = coalesce(CONFIG.SUMMARY.RESULTS_DIR, '.')
@@ -14,22 +10,13 @@ export default (application, testName) => (data) => {
     const outputFilePrefix = `${application}_${testName}`
 
     console.log(
-        `TEST DETAILS: [Time to complete test: ${data.state.testRunDurationMs} ms, Environment target: ${CONFIG.TARGET_ENV}, Scenario test type: ${CONFIG.SCENARIOS.TYPES}, Request processed: ${data.metrics.http_reqs.values.count}, Request OK: ${data.metrics.http_req_failed.values.fails}, ERRORS: ${data.metrics.http_req_failed.values.passes}]`
+        `TEST DETAILS: [Time to complete test: ${data.state.testRunDurationMs} ms, Environment target: ${CONFIG.TARGET_ENV}, Scenario: rampingGrowingArrivalRate, Request processed: ${data.metrics.http_reqs.values.count}, Request OK: ${data.metrics.http_reqs.values.count - data.metrics.http_req_failed.values.count}, Errors: ${data.metrics.http_req_failed.values.count}]`
     )
 
-    if (CONFIG.SCENARIOS.TYPES.indexOf('rampingArrivalRate') > -1) {
-        printRampingConfig(
-            'rampingArrivalRate',
-            rampingArrivalRate.rampingArrivalRate.stages 
-        )
-    }
-
-    if (CONFIG.SCENARIOS.TYPES.indexOf('rampingGrowingArrivalRate') > -1) {
-        printRampingConfig(
-            'rampingGrowingArrivalRate',
-            rampingGrowingArrivalRate.rampingGrowingArrivalRate.stages
-        )
-    }
+    printRampingConfig(
+        'rampingGrowingArrivalRate',
+        rampingGrowingArrivalRate.rampingGrowingArrivalRate.stages
+    )
 
     const outputJUnitFile = `${outputDir}/results/${outputFilePrefix}-result.xml`
     const outputTextSummaryFile = `${outputDir}/results/${outputFilePrefix}-summary.txt`
@@ -41,20 +28,17 @@ export default (application, testName) => (data) => {
 
     return {
         stdout: textSummary(data, { indent: ' ', enableColors: true }),
-        [outputTextSummaryFile]: textSummary(data, {
-            indent: ' ',
-            enableColors: false,
-        }),
+        [outputTextSummaryFile]: textSummary(data, { indent: ' ', enableColors: false }),
         [outputHtmlSummaryFile]: htmlReport(data),
         [outputJUnitFile]: jUnit(data, { name: outputFilePrefix }),
     }
 }
 
 function printRampingConfig(scenarioName, customStages) {
-    let stringRamping = `[${scenarioName}] Ramping iterations for stage : { `
-
-    for (let i = 0; i < customStages.length - 1; i++) {
-        stringRamping += `${customStages[i].target}, `
+    let stringRamping = `[${scenarioName}] Ramping iterations for stages: { `
+    for (let i = 0; i < customStages.length; i++) {
+        stringRamping += `${customStages[i].target}${i < customStages.length - 1 ? ', ' : ''}`
     }
-
+    stringRamping += ' }'
+    console.log(stringRamping)
 }
