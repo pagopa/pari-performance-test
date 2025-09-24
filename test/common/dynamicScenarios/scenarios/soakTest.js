26c46e48e961a53cd6fb43bd5dc1256bc41fad2d
DanieleRanaldo <Daniele.Ranaldo@nttdata.com>
2025-09-24 12:06:15 +0200

    test 85 rate

--- test/common/dynamicScenarios/scenarios/soakTest.js
+++ test/common/dynamicScenarios/scenarios/soakTest.js
@@ -1,7 +1,7 @@
 export default {
     soakTest: {
         executor: 'constant-arrival-rate',
-        rate: 100,
+        rate: 85,
         timeUnit: '1s',
         duration: '5m',
         preAllocatedVUs: 10,
