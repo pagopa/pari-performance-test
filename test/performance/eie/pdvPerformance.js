925b801430e91f4319996ea1808f4c2abd67b156
Diego Mauricio Lagos <diego.lagosmorales@pagopa.it>
2025-09-30 15:25:54 +0200

    minro fix

--- test/performance/eie/pdvPerformance.js
+++ test/performance/eie/pdvPerformance.js
@@ -6,24 +6,24 @@ import { check } from 'k6';
 import { randomString } from 'https://jslib.k6.io/k6-utils/1.2.0/index.js';
 
 export const options = {
-	discardResponseBodies: true,
-	scenarios: {
-		contacts: {
-			executor: 'constant-arrival-rate',
-			rate: __ENV.RATE,
-			timeUnit: '1s'
-		}
-	},
+    discardResponseBodies: true,
+    scenarios: {
+        contacts: {
+            executor: 'constant-arrival-rate',
+            rate: __ENV.RATE,
+            timeUnit: '1s'
+        }
+    },
     thresholds: {
         checks: ['rate>0.99'],
     }
 };
 
-export default function() {
+export default function () {
     let body = {
         pii: randomString(8, 'abcdefghijklmnopqrstuvwxyz01234567890')
     };
-    
+
     let resp = http.put(
         `${__ENV.URL}/tokens`,
         JSON.stringify(body),
@@ -32,6 +32,6 @@ export default function() {
                 'Content-Type': 'application/json'
             }
         });
-    
-    check(resp, { 'status was 200': (r) => r.status === 200 } );
+
+    check(resp, { 'status was 200': (r) => r.status === 200 });
 }
