bf2b19923131a76ad5d222d98b6e7daf9f2e5382
and-mora <andrea.morabito@pagopa.it>
2025-10-08 11:17:25 +0200

    change api status

--- test/common/api/onboardingStatus.js
+++ test/common/api/onboardingStatus.js
@@ -7,7 +7,7 @@ import http from 'k6/http'
  * @param {string} token - The Bearer token for authorization.
  * @returns {Response} - The k6 HTTP response object.
  */
-export function getOnboardingStatus(baseUrl, initiativeId, token, expectedStatuses = [200]) {
+export function getOnboardingStatus(baseUrl, initiativeId, token) {
     const url = `${baseUrl}/onboarding/${initiativeId}/status`
     const headers = {
         'X-Api-Version': 'v1',
@@ -18,8 +18,7 @@ export function getOnboardingStatus(baseUrl, initiativeId, token, expectedStatus
     }
 
     const params = {
-        headers,
-        expectedStatuses,
+        headers
     }
 
     return http.get(url, params)
