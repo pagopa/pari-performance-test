cd8854cce1b3d34eba7ee0bc33e6f056b0497732
and-mora <andrea.morabito@pagopa.it>
2025-10-16 18:30:57 +0200

    format

--- test/performance/idpay/onboardingTest.js
+++ test/performance/idpay/onboardingTest.js
@@ -1,4 +1,4 @@
-import { textSummary } from 'https://jslib.k6.io/k6-summary/0.0.2/index.js'
+import { textSummary } from 'https://jslib.k6.io/k6-summary/0.0.2/index.js';
 import { htmlReport } from 'https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js';
 
 import { check } from 'k6';
